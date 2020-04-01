b5b767ca0d2eb9897ea5c01699a7c87083b856ef
morseck00 <morseck00@gmail.com>
2020-04-01 18:35:16 +0000

    mor commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Fronthaby'`, () => {
+  it(`should have as title 'Habycall'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Fronthaby');
+    expect(app.title).toEqual('Habycall');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Fronthaby!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Habycall!');
   });
 });
